5b1d7a9922622e151e661cb6fbd470191faddf83
Ben <WBHankins93@Bens-MacBook-Air.local>
2018-09-11 15:06:38 -0500

    Loop lab from 9-11

---
+++ js/lab2.js
@@ -0,0 +1,33 @@
+//SyntaxError: missing ) after argument list
+
+console.log("Making your way in the world today takes everything you've got.");
+console.log("Taking a break from all your worries, sure would help a lot.");
+console.log("Wouldn't you like to get away?");
+console.log("Sometimes you want to go");
+console.log("Where everybody knows your name,");
+console.log("and they're always glad you came.");
+console.log("You wanna be where you can see,");
+console.log("our troubles are all the same");
+console.log("You wanna be where everybody knows");
+console.log("Your name.");
+
+let num = 1;
+
+while(num <= 1000) {
+  console.log('Ginger chocolate honey patties');
+  num++;
+}
+
+let x = 1;
+
+while(x <= 1000) {
+  console.log(x);
+  x++;
+}
+
+let i = 1;
+
+while(i <= 1000) {
+  console.log('the number is: ' + i);
+  i++;
+}
